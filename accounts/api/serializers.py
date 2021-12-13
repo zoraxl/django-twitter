08d027be0fbf0f6eac45916e0bf5f9be30dff834
Zora Li <53531227+zoraxl@users.noreply.github.com>
2021-12-12 20:03:32 -0500

    add tests for tweets api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -2,10 +2,16 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers, exceptions
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('id','username', 'email')
+
+class UserSerializerForTweet(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
 
 class SignupSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=20, min_length=6)
--- twitter/urls.py
+++ twitter/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts.api import views
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
 router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
+router.register(r'api/tweets', TweetViewSet, basename='tweets')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
