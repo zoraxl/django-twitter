27de23c435b31b335e433953336b7e05a5e9f4e3
Zora Li <53531227+zoraxl@users.noreply.github.com>
2022-01-08 19:10:49 -0500

    adding likes model and admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,15 +1,25 @@
-from django.contrib.auth.models import User
-from django.test import TestCase
-from tweets.models import Tweet
+from testing.testcases import TestCase
 from datetime import timedelta
 from utils.time_helpers import utc_now
 
-# Create your tests here.
+
 class TweetTests(TestCase):
+    def setUp(self):
+        self.linghu = self.create_user('linghu')
+        self.tweet = self.create_tweet(self.linghu, content='Jiuzhang Dafa Hao')
 
     def test_hours_to_now(self):
-        zora = User.objects.create_user(username='zorali-test')
-        tweet = Tweet.objects.create(user=zora, content='here is a test message.')
-        tweet.created_at = utc_now() - timedelta(hours=10)
-        tweet.save()
-        self.assertEqual(tweet.hours_to_now, 10)
+        self.tweet.created_at = utc_now() - timedelta(hours=10)
+        self.tweet.save()
+        self.assertEqual(self.tweet.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(self.linghu, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        self.create_like(self.linghu, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        dongxie = self.create_user('dongxie')
+        self.create_like(dongxie, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 2)
