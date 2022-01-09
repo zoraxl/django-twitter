8e63f0b443395974897d2656cc02041c27420bd5
Zora Li <53531227+zoraxl@users.noreply.github.com>
2022-01-08 23:12:34 -0500

    18-inject-likes-info-to-other-apis

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,13 +1,36 @@
 from rest_framework import serializers
 from accounts.api.serializers import UserSerializer
 from comments.api.serializers import CommentSerializer
+from likes.api.serializers import LikeSerializer
+from likes.services import LikeService
 from tweets.models import Tweet
+from likes.services import LikeService
+
 class TweetSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    has_liked = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Tweet
-        fields = ('id', 'user', 'created_at', 'content')
+        fields = (
+            'id',
+            'user',
+            'content',
+            'created_at',
+            'comments_count',
+            'likes_count',
+            'has_liked',
+        )
+    def get_likes_count(self, obj):
+        return obj.like_set.count()
+
+    def get_comments_count(self, obj):
+        return obj.comment_set.count()
+
+    def get_has_liked(self, obj):
+        return LikeService.has_liked(self.context['request'].user, obj)
 
 class TweetSerializerForCreate(serializers.ModelSerializer):
     content = serializers.CharField(min_length=6, max_length=140)
@@ -22,12 +45,21 @@ class TweetSerializerForCreate(serializers.ModelSerializer):
         tweet = Tweet.objects.create(user=user, content=content)
         return tweet
 
-
-class TweetSerializerWithComments(serializers.ModelSerializer):
-    user = UserSerializer()
-    # <HOMEWORK> 使用 serialziers.SerializerMethodField 的方式实现 comments
+class TweetSerializerForDetail(TweetSerializer):
     comments = CommentSerializer(source='comment_set', many=True)
+    likes = LikeSerializer(source='like_set', many=True)
 
     class Meta:
         model = Tweet
-        fields = ('id', 'user', 'comments', 'created_at', 'content')
+        fields = (
+            'id',
+            'user',
+            'comments',
+            'created_at',
+            'content',
+            'likes',
+            'comments',
+            'likes_count',
+            'comments_count',
+            'has_liked',
+        )
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from tweets.api.serializers import (
     TweetSerializer,
     TweetSerializerForCreate,
-    TweetSerializerWithComments,
+    TweetSerializerForDetail,
 )
 from tweets.models import Tweet
 from utils.decorators import required_params
@@ -30,8 +30,10 @@ class TweetViewSet(viewsets.GenericViewSet,
     def retrieve(self, request, *args, **kwargs):
         # <HOMEWORK 1> 通过某个 query 参数 with_all_comments 来决定是否需要带上所有 comments
         # <HOMEWORK 2> 通过某个 query 参数 with_preview_comments 来决定是否需要带上前三条 comments
-        tweet = self.get_object()
-        return Response(TweetSerializerWithComments(tweet).data)
+        return Response(TweetSerializerForDetail(
+            self.get_object(),
+            context={'request': request},
+        ).data)
 
     @required_params(params=['user_id'])
     def list(self, request, *args, **kwargs):
@@ -45,10 +47,17 @@ class TweetViewSet(viewsets.GenericViewSet,
             user_id=request.query_params['user_id']
         ).order_by('-created_at')
 
-        serializer = TweetSerializer(tweets, many=True)
+        serializer = TweetSerializer(
+            tweets,
+            context={'request':request},
+            many=True,
+        )
         return Response({'tweets': serializer.data})
 
     def create(self, request, *args, **kwargs):
+        """
+        重载 create 方法，因为需要默认用当前登陆用户作为tweet.user
+        """
         serializer = TweetSerializerForCreate(
             data = request.data,
             context={'request': request},
@@ -63,7 +72,8 @@ class TweetViewSet(viewsets.GenericViewSet,
         # save will call create method in TweetSerializerForCreate
         tweet = serializer.save()
         NewsFeedService.fanout_to_followers(tweet)
-        return Response(TweetSerializer(tweet).data, status=201)
+        serializer = TweetSerializer(tweet, context={'request': request})
+        return Response(serializer.data, status=201)
 
 
 
